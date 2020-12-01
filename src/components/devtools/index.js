d3e44b68fabfb46406399092f738e63ada88441f
liaohuanyu <liaohuanyu@baidu.com>
2020-12-01 15:46:07 +0800

    feat: 初版

--- src/components/devtools/index.js
+++ src/components/devtools/index.js
@@ -2,18 +2,38 @@
  * @file Task Client Addon
 */
 
-import './index.less';
+import 'whatwg-fetch';
+import Home from './Home.san';
 
 export default {
     template: /* html */`
         <div class="san-devtools-wrapper">
-           demo
+           <sand-home 
+                s-if="{{showHome}}"
+                config="{{config}}"
+            />
         </div>
     `,
 
     initData() {
         return {
             // TODO: some todo
+            showHome: false,
+            config: null
         };
+    },
+
+    components: {
+        'sand-home': Home
+    },
+
+    attached() {
+        let _self = this;
+        window.fetch("http://172.24.198.107:8899/jsonp/").then(res => {
+            return res.json();
+        }).then(function(config) {
+            _self.data.set('showHome', true);
+            _self.data.set('config', config);
+        });
     }
 };
