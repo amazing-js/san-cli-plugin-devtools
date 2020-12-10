afa4dbff067a5c5290ad5c2c074677475502b313
jinzhan <jinzhan@baidu.com>
2020-12-10 10:53:35 +0800

    fix: 向html中注入devtools的backend.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-// const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 const SanDevToolsPlugin = require('./lib/SanDevToolsPlugin');
-
-// For Test
-// process.env.SAND_URL_INJECT_ENABLED = true;
-// process.env.SAND_BACKEND_URL = 'http://localhost:8888/test-inject.js';
 
 module.exports = {
     id: 'san-cli:plugin-san-devtools',
     apply(api, projectOptions, options = {}) {
-        console.log('process.env.SAND_URL_INJECT_ENABLED:', process.env.SAND_URL_INJECT_ENABLED);
         // 只有sand开启了并且san cli允许添加，才进行注入
         if (!process.env.SAND_BACKEND_URL || !process.env.SAND_URL_INJECT_ENABLED) {
             return;
         }
+
+        console.log('SAND_BACKEND_URL', process.env.SAND_BACKEND_URL);
+
         api.chainWebpack(webpackConfig => {
             webpackConfig.plugin('san-devtools').use(SanDevToolsPlugin, [{
-                files: process.env.SAND_BACKEND_URL,
                 replacements: [
-                    function (inHtml) {
-                        // TODO: 这里完成script注入逻辑
-                        return inHtml.replace('<title>', '<!--' + process.env.SAND_BACKEND_URL + '--><title>');
+                    function (html) {
+                        // 如果是smarty模板
+                        const smartyTag = '{%block name="__head_asset"%}';
+                        const script = `<script src="${process.env.SAND_BACKEND_URL}"></script>`;
+                        return ~html.indexOf(smartyTag) ? html.replace(smartyTag, `${smartyTag}${script}`)
+                         : html.replace('</title>', `</title>${script}`);
                     }
                 ]
             }]);
