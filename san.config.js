3dbdbd1ecf1f6446382eef8f075a0bb18087e132
liaohuanyu <liaohuanyu@baidu.com>
2021-02-01 16:50:19 +0800

    fix: 修改依赖,配置less-loader

--- san.config.js
+++ san.config.js
@@ -9,5 +9,37 @@ module.exports = {
     ...clientAddonConfig({
         id: 'san.webpack.client-view.devtools',
         port: 8952
-    })
+    }),
+    chainWebpack: config => {
+        config.module
+            .rule('less')
+            .oneOf('normal')
+            .use('less-loader')
+            .tap(options => {
+                // 修改它的选项...
+                delete options.javascriptEnabled;
+                delete options.compress;
+                return Object.assign({}, options, {
+                    lessOptions: {
+                        javascriptEnabled: true,
+                        compress: false
+                    }
+                });
+            })
+        config.module
+            .rule('less')
+            .oneOf('normal-modules')
+            .use('less-loader')
+            .tap(options => {
+                // 修改它的选项...
+                delete options.javascriptEnabled;
+                delete options.compress;
+                return Object.assign({}, options, {
+                lessOptions: {
+                    javascriptEnabled: true,
+                    compress: false
+                }
+                });
+            })
+    }
 };
